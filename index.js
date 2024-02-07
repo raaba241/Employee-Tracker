00d1489ef4ea0b29d30e6e03c75f74d554351540
unknown <raaba21@gmail.com>
2024-02-06 21:26:38 -0500

    added all functions corresponding the choices users make but functions are still empty

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function mainQuestions() {
         .prompt([
             {
                 type: 'list',
-                name: 'reptile',
+                name: 'choice',
                 message: 'What would you like to do? (Use arrow keys to navigate list)',
                 choices: ['View All Employees',
                     'Add Employee',
@@ -20,11 +20,46 @@ function mainQuestions() {
             },
         ])
         .then(answers => {
-            
+            if (answers.choice === "View All Employees"){
+                viewAllEmployees()
+            }
+            else if (answers.choice === "Add Employee"){
+                addEmployees()
+            }
+            else if (answers.choice === "Update Employee Role"){
+                updateEmployeeRole()
+            }
+            else if (answers.choice === "View All Roles"){
+                viewAllRoles()
+            }
+            else if (answers.choice === "Add Role"){
+                addRole()
+            }
+            else if (answers.choice === "View All Departments"){
+                viewAllDepartments()
+            }
+            else if (answers.choice === "Add Department"){
+                addDepatment()
+            }
+            else if (answers.choice === "Quit"){
+                quit()
+            }
+         
+
         })
         .catch(error => {
             console.error('An error occurred:', error);
         });
 }
 
+function viewAllEmployees(){}
+function addEmployees(){}
+function updateEmployeeRole(){}
+function viewAllRoles(){}
+function addRole(){}
+function viewAllDepartments(){}
+function addDepatment(){}
+function quit(){}
+
 mainQuestions()
+
